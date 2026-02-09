4f1620ddf712e65850121834edc8649372fcf3a9
Luigi32ND <harrydgleeson@gmail.com>
2026-02-09 12:04:26 +0000

    demo push1

--- Demo1.java
+++ Demo1.java
@@ -1,10 +1,14 @@
 public class Demo1 {
-	
-			public int method1(int number) {
-		
-				int square = number * number;
+
+			public float method1(float number){
+				float square = number * number;
 				return square;
 
 			} // end method1
-	
+public boolean method2(char letter,float number){
+	if ((letter == "A") && (number == 12.0))
+	return false;
+	return true;
+}//end method2
+
 }// end Demo1
