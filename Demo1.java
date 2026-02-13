bd38e813bfea7eb90c6073ba3b6720f89117b52f
caradaly65-tech <caradaly65@gmail.com>
2026-02-13 21:05:00 +0000

    Update Demo1

--- Demo1.java
+++ Demo1.java
@@ -1,10 +1,16 @@
 public class Demo1 {
 	
-			public int method1(int number) {
+	public int method1(int number) {
 		
-				int square = number * number;
-				return square;
+		int square = number * number;
+		return square;
 
-			} // end method1
+	} // end method1
 	
+	public char method3() {
+		
+			return 'Z';
+		
+		} // end method3
+
 }// end Demo1
